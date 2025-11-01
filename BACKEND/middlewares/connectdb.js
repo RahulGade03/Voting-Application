44397ade793bbe7f8e5d7990695c12f20b2f3b82
RahulGade03 <rahulgade2004@gmail.com>
2025-11-01 23:08:55 +0530

    changed mongodb configuration

--- BACKEND/middlewares/connectdb.js
+++ BACKEND/middlewares/connectdb.js
@@ -1,13 +1,25 @@
-import mongoose from 'mongoose';
+// middlewares/connectdb.js
+import mongoose from "mongoose";
 
-const connectDB = async() => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log ('MongoDB connected successfully!');
-    }
-    catch (error) {
-        console.log (error);
-    }
-}
+let isConnected = false; // Global flag
+
+const connectDB = async () => {
+  if (isConnected) { // Already connected
+    return;
+  }
+
+  try {
+    const db = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    isConnected = db.connections[0].readyState;
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.error("MongoDB connection failed:", error.message);
+    throw error;
+  }
+};
 
 export default connectDB;
